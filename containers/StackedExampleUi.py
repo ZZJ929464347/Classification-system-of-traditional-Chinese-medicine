57cd661b60ec45880e4058e86e90c38460ea5276
ZZJ929464347 <929464347@qq.com>
2020-03-04 14:10:04 +0800

    用qss技术优化了界面的展示效果，同时将系统需加载的icon和查询界面需展示的中药图片资源上传

--- containers/StackedExampleUi.py
+++ containers/StackedExampleUi.py
@@ -13,30 +13,35 @@ import tempfile
 from PyQt5.QtCore import QThread, pyqtSignal
 from CNNnet.predictSingleImg import CNN
 from Mysql.DataBase import Mysql
+import qtawesome
 
 
 class HomePageUi(QWidget):
     def __init__(self, parent=None):
         super(HomePageUi, self).__init__(parent)
 
-
     def setUpUi(self):
-        #设置窗口初始位置和大小
-        self.setGeometry(300,100,1100,800)
-        self.setWindowTitle('StackedWidget 例子')
+        # 设置窗口初始位置和大小
+        self.setGeometry(300, 100, 1200, 800)
+        self.setWindowTitle('中药饮片分类系统')
+        self.setWindowIcon(QIcon('../source/img/logo.jpg'))
 
-        #创建列表窗口，添加条目
-        self.leftlist=QListWidget()
-        self.leftlist.insertItem(0,'爬虫')
-        self.leftlist.insertItem(1,'图像预处理')
-        self.leftlist.insertItem(2,'图像预测')
-        self.leftlist.insertItem(3,'中药查询')
+        # 创建列表窗口，添加条目
+        self.leftlist = QListWidget()
+        item1 = QtWidgets.QListWidgetItem(qtawesome.icon('fa.sellsy', color='white'), '爬虫功能')
+        item2 = QtWidgets.QListWidgetItem(qtawesome.icon('fa.hand-o-right', color='white'), '图像处理')
+        item3 = QtWidgets.QListWidgetItem(qtawesome.icon('fa.plane', color='white'), '图像预测')
+        item4 = QtWidgets.QListWidgetItem(qtawesome.icon('fa.search', color='white'), '中药查询')
+        self.leftlist.insertItem(0, item1)
+        self.leftlist.insertItem(1, item2)
+        self.leftlist.insertItem(2, item3)
+        self.leftlist.insertItem(3, item4)
 
-        #创建四个小控件
-        self.stack1=QWidget()
-        self.stack2=QWidget()
-        self.stack3=QWidget()
-        self.stack4=QWidget()
+        # 创建四个小控件
+        self.stack1 = QWidget()
+        self.stack2 = QWidget()
+        self.stack3 = QWidget()
+        self.stack4 = QWidget()
 
         # self.stack1UI()
         self.stack1UI()
@@ -44,8 +49,8 @@ class HomePageUi(QWidget):
         self.stack3UI()
         self.stack4UI()
 
-        #在QStackedWidget对象中填充了四个子控件
-        self.stack=QStackedWidget(self)
+        # 在QStackedWidget对象中填充了四个子控件
+        self.stack = QStackedWidget(self)
         self.stack.addWidget(self.stack1)
         self.stack.addWidget(self.stack2)
         self.stack.addWidget(self.stack3)
@@ -55,17 +60,67 @@ class HomePageUi(QWidget):
         splitter = QSplitter(Qt.Horizontal)
         splitter.addWidget(self.leftlist)
         splitter.addWidget(self.stack)
-        splitter.setStretchFactor(0, 1)
+        splitter.setStretchFactor(0, 5)
         splitter.setStretchFactor(1, 13)
 
-        #水平布局，添加部件到布局中
-        HBox=QHBoxLayout()
+        # 水平布局，添加部件到布局中
+        HBox = QHBoxLayout()
         HBox.addWidget(splitter)
         self.setLayout(HBox)
 
         # 添加信号与槽
         self.leftlist.currentRowChanged.connect(self.display)
 
+        qssStyle = '''
+                QListWidget
+                {
+                    border:1px solid gray;
+                    background:	Gray; 
+                    outline:0px;
+                    border-radius:10px;
+                }
+                QListWidget::item
+                {
+                    color:white;
+                    height:100px;
+                    border:0px solid gray;
+                }
+                QListWidget::Item:hover{background:skyblue;border-radius:10px;}
+                QListWidget::item:selected{background:lightgray; color:red;border-radius:10px;}
+                QListWidget::item:selected:!active{border-width:0px; background:lightgreen;border-radius:10px;}
+
+                QPushButton{
+                    border-radius:10px;
+                    background:LightGray;
+                    color:black;
+                    font-size:13px;
+                    height:30px;
+                    text-align:center;
+                }
+                QPushButton:hover{
+                    color:black;
+                    border:1px solid #F3F3F5;
+                    border-radius:10px;
+                    background:DarkGray;
+                }
+
+                QLineEdit{
+                    border:1px solid gray;
+                    width:300px;
+                    border-radius:10px;
+                    padding:2px 4px;
+                }
+
+                QTextEdit{
+
+                    border-radius:20px;
+                }
+                '''
+        # 加载全局的样式
+        self.setStyleSheet(qssStyle)
+        # 设置背景透明
+        self.setWindowOpacity(0.95)
+        # self.setWindowFlag(Qt.FramelessWindowHint)
 
     def stack1UI(self):
         # 设置栅格布局
@@ -94,6 +149,12 @@ class HomePageUi(QWidget):
 
         # 设置进度显示Text
         self.spiderInfo = QTextEdit()
+        self.spiderInfo.setStyleSheet(
+            '''
+            background:	Gainsboro;
+            border-radius:20px;
+           '''
+        )
         grid1.addWidget(self.spiderInfo, 1, 8, 20, 4)
 
         # 设置搜索中药饮片名称输入框
@@ -102,7 +163,7 @@ class HomePageUi(QWidget):
 
         # 设置图片保存目录位置的输入框
         self.lineEditSpiderSaveDirectory = QLineEdit()
-        grid1.addWidget(self.lineEditSpiderSaveDirectory, 3, 2, 1, 3)
+        grid1.addWidget(self.lineEditSpiderSaveDirectory, 3, 2, 1, 4)
 
         # 设置搜索的开始爬取页数输入框
         self.lineEditStartPage = QLineEdit()
@@ -117,7 +178,7 @@ class HomePageUi(QWidget):
         buttonSpiderSaveDirectory.setObjectName("buttonSpiderSaveDirectory")
         buttonSpiderSaveDirectory.setText("选择保存目录")
         buttonSpiderSaveDirectory.clicked.connect(self.getSpiderSaveDirectory)
-        grid1.addWidget(buttonSpiderSaveDirectory, 3, 5, 1, 1)
+        grid1.addWidget(buttonSpiderSaveDirectory, 3, 6, 1, 1)
 
         # 设置开始爬取按钮1
         self.buttonStartSpider = QPushButton(self)
@@ -126,12 +187,8 @@ class HomePageUi(QWidget):
         self.buttonStartSpider.clicked.connect(self.startSpider)
         grid1.addWidget(self.buttonStartSpider, 18, 2, 1, 4)
 
-
-
-
         # 将栅格布局加入窗体
         self.stack1.setLayout(grid1)
-
 
     # 图像预处理模块
     def stack2UI(self):
@@ -163,11 +220,17 @@ class HomePageUi(QWidget):
 
         # 显示图片的label
         self.imageLabel = QLabel(self)
-        self.imageLabel.setText("                  显示图片")
+        self.imageLabel.setText("                                               显示图片")
         self.imageLabel.setFixedSize(800, 700)
         self.imageLabel.setStyleSheet("QLabel{background:white;}"
                                       "QLabel{color:rgb(300,300,300,120);font-size:10px;font-weight:bold;font-family:宋体;}"
                                       )
+        self.imageLabel.setStyleSheet(
+            '''
+            background:	Gainsboro;
+            border-radius:40px;
+           '''
+        )
 
         # 将图片的label加入栅格布局中
         grid2.addWidget(self.imageLabel, 3, 2, 6, 5)
@@ -219,11 +282,17 @@ class HomePageUi(QWidget):
 
         # 显示预测图片的label
         self.predictImageLabel = QLabel(self)
-        self.predictImageLabel.setText("                  显示预测图片")
+        self.predictImageLabel.setText("                                             显示预测图片")
         self.predictImageLabel.setFixedSize(800, 700)
         self.predictImageLabel.setStyleSheet("QLabel{background:white;}"
-                                      "QLabel{color:rgb(300,300,300,120);font-size:10px;font-weight:bold;font-family:宋体;}"
-                                      )
+                                             "QLabel{color:rgb(300,300,300,120);font-size:10px;font-weight:bold;font-family:宋体;}"
+                                             )
+        self.predictImageLabel.setStyleSheet(
+            '''
+            background:	Gainsboro;
+            border-radius:40px;
+           '''
+        )
 
         # 将预测图片的label加入栅格布局中
         grid3.addWidget(self.predictImageLabel, 3, 2, 6, 5)
@@ -257,38 +326,49 @@ class HomePageUi(QWidget):
 
         # 设置中药搜索编辑框
         self.lineEditSearchMedical = QLineEdit()
+        self.lineEditSearchMedical.setPlaceholderText("请输入中药名称")
 
         grid4.addWidget(self.lineEditSearchMedical, 1, 1, 1, 7)
         grid4.addWidget(buttonSearchMedical, 1, 8, 1, 1)
 
         # 显示中药搜索结果图片展示的label
         self.searchResultImageShowLabel = QLabel(self)
-        self.searchResultImageShowLabel.setText("                  显示搜索结果图片")
+        self.searchResultImageShowLabel.setText("               显示搜索结果图片")
         self.searchResultImageShowLabel.setFixedSize(360, 360)
         self.searchResultImageShowLabel.setStyleSheet("QLabel{background:white;}"
-                                             "QLabel{color:rgb(300,300,300,120);font-size:10px;font-weight:bold;font-family:宋体;}"
-                                             )
+                                                      "QLabel{color:rgb(300,300,300,120);font-size:10px;font-weight:bold;font-family:宋体;}"
+                                                      )
+        self.searchResultImageShowLabel.setStyleSheet(
+            '''
+            background:	Gainsboro;
+            border-radius:30px;
+           '''
+        )
         grid4.addWidget(self.searchResultImageShowLabel, 3, 1, 3, 3)
 
         # 设置中药搜索结果文字展示编辑框
         self.lineEditResultShow = QTextEdit()
+        self.lineEditResultShow.setStyleSheet(
+            '''
+            background:	Gainsboro;
+            border-radius:20px;
+           '''
+        )
         grid4.addWidget(self.lineEditResultShow, 3, 4, 7, 5)
 
         self.stack4.setLayout(grid4)
 
     # 槽-----------------------------界面切换
-    def display(self,i):
-        #设置当前可见的选项卡的索引
+    def display(self, i):
+        # 设置当前可见的选项卡的索引
         self.stack.setCurrentIndex(i)
-
 
     # 槽-----------------------------选择爬虫保存图像文件夹
     def getSpiderSaveDirectory(self):
         self.getSpiderSaveDirectoryPath = QFileDialog.getExistingDirectory(self, "选取文件夹", "./")  # 起始路径
         self.lineEditSpiderSaveDirectory.setText(self.getSpiderSaveDirectoryPath)
 
-
-    #槽-----------------------------获取预处理图像文件夹
+    # 槽-----------------------------获取预处理图像文件夹
     def getDirectory(self):
         self.getChooseDirectory = QFileDialog.getExistingDirectory(self, "选取文件夹", "./")  # 起始路径
         self.lineEditChooseDirectory.setText(self.getChooseDirectory)
@@ -301,7 +381,6 @@ class HomePageUi(QWidget):
         except:
             print("预处理图像文件夹为空")
 
-
     # 槽-----------------------------获取保存图像文件夹
     def saveDirectory(self):
         self.getSaveDirectory = QFileDialog.getExistingDirectory(self, "选取文件夹", "./")  # 起始路径
@@ -313,15 +392,13 @@ class HomePageUi(QWidget):
         except:
             print("保存图像文件夹为空")
 
-
     # 槽-----------------------------选择待预测中药图像文件位置
     def getPredictImgPath(self):
-        self.getPredictImgPath, _  = QFileDialog.getOpenFileName(self, "选取文件", "./")  # 起始路径
+        self.getPredictImgPath, _ = QFileDialog.getOpenFileName(self, "选取文件", "./")  # 起始路径
         self.lineEditChoosePredictImg.setText(self.getPredictImgPath)
         print(self.getPredictImgPath)
         jpg = QPixmap(self.getPredictImgPath).scaled(self.predictImageLabel.width(), self.predictImageLabel.height())
         self.predictImageLabel.setPixmap(jpg)
-
 
     # 槽-----------------------------加载模型
     def loadModel(self):
@@ -329,25 +406,23 @@ class HomePageUi(QWidget):
         self.cnn.loadModel()
         QMessageBox.about(self, "提示", "模型加载成功")
 
-
     # 槽-----------------------------预测中药图像
     def predictImg(self):
         result = self.cnn.predict(self.getPredictImgPath)
-        QMessageBox.about(self, "预测结果", result)
-
+        QMessageBox.about(self, "提示", "预测结果：" + result)
 
     # 槽-----------------------------搜索中药
     def serchMedical(self):
         mysql = Mysql()
         result = mysql.show(self.lineEditSearchMedical.text())
-        if  result == []:
+        if result == []:
             QMessageBox.about(self, "提示", "无结果，请重新查询")
         else:
             name, info, imgPath = result
             print(imgPath)
             # 显示搜索的中药图片
             jpg = QPixmap(imgPath).scaled(self.searchResultImageShowLabel.width(),
-                                           self.searchResultImageShowLabel.height())
+                                          self.searchResultImageShowLabel.height())
             self.searchResultImageShowLabel.setPixmap(jpg)
             # 显示搜索的中药信息
             self.lineEditResultShow.setText(info)
@@ -366,24 +441,20 @@ class HomePageUi(QWidget):
         self.thread_1.trigger.connect(self.print_in_textEdit)
         self.thread_1.start()
 
-
     # 将爬虫的控制台信息显示在spiderInfo控件上
     def print_in_textEdit(self, msg):
         self.spiderInfo.append(msg)
         self.thread_1.exit()
-
 
     # 从文件夹中获取图像文件名称，但是不是有序的
     def randomGetImage(self, directoryPath):
         result = os.listdir(directoryPath)
         return result
 
-
     # 获取下一张图片
     def nextImgFile(self):
         self.imgNumber = self.imgNumber + 1
         self.getNewLabelImage()
-
 
     # 获取文件夹里面的图片，并显示到self.imageLabel上
     def getNewLabelImage(self):
@@ -396,7 +467,6 @@ class HomePageUi(QWidget):
         # print(self.imagePath)
         # print(self.getChooseDirectory)
 
-
     # 删除文件
     def delFile(self):
         if os.path.exists(self.imageNowPath):  # 如果文件存在
@@ -408,7 +478,6 @@ class HomePageUi(QWidget):
         else:
             print('no such file:%s' % self.imageNowPath)
 
-
     ####### 初始化labelCombobox打标签的下拉选项框数据  增加单项元素，不带数据  #########
     def init_labelCombobox(self):
         items_list = ['苍术-----0', '苍耳子---1', '决明子---2', '枳实-----3', '天花粉---4', '黄连-----5']
@@ -416,12 +485,12 @@ class HomePageUi(QWidget):
             self.labelCombobox.addItem(items_list[i])
         self.labelCombobox.setCurrentIndex(-1)
 
-
     # 当点击打标签的多选框选项时，触发事件
     def on_labelCombobox_Activate(self, index):
         if self.imgNumber < len(self.imgNameList):
             suffix = self.get_suffix(self.imageNowPath)
-            saveImgPath = self.getSaveDirectory + "/" + str(self.labelCombobox.currentIndex()) + "-" + str(self.saveDirectoryImgNumber) + suffix
+            saveImgPath = self.getSaveDirectory + "/" + str(self.labelCombobox.currentIndex()) + "-" + str(
+                self.saveDirectoryImgNumber) + suffix
             # 移动图片文件
             shutil.move(self.imageNowPath, saveImgPath)
             # 保存文件夹数+1
@@ -436,7 +505,6 @@ class HomePageUi(QWidget):
         # print(self.labelCombobox.currentIndex())
         # print(self.labelCombobox.currentText())
 
-
     # 获取后缀名
     def get_suffix(self, name):
         # \.是匹配.的意思 ^\.是匹配不是.的字符 [^\.]是匹配任意不是.的字符当中的一个   [^\.]*匹配0个或任意多个不是.的字符  匹配到$结束符    search方法扫描整个字符串并返回第一个成功的匹配
@@ -447,7 +515,6 @@ class HomePageUi(QWidget):
             return m.group(0)
         else:
             return '.jpeg'
-
 
 
 # 设置一个线程，用来实时显示爬虫的最新信息
@@ -462,7 +529,8 @@ class ThreadCrawl(QThread):
         self.saveDirectoryPath = saveDirectoryPath
 
     def run(self):
-        shell_cmd = "python ../爬虫.py" + " " + str(self.name) + " " + str(self.spider_page_num) + " " + str(self.start_page) + " " + str(self.saveDirectoryPath)
+        shell_cmd = "python ../爬虫.py" + " " + str(self.name) + " " + str(self.spider_page_num) + " " + str(
+            self.start_page) + " " + str(self.saveDirectoryPath)
         cmd = shlex.split(shell_cmd)
         p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         while p.poll() is None:
