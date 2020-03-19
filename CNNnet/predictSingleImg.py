c47c5bac9753cc901fc3476ec3bec958fa5a9d9f
ZZJ929464347 <929464347@qq.com>
2020-03-19 22:09:00 +0800

    在预测的时候，去掉了softmax步骤，直接采用了max

--- CNNnet/predictSingleImg.py
+++ CNNnet/predictSingleImg.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from torchvision import transforms, models
-from torch.autograd import Variable as V
+from torch.autograd import Variable
 import torch as t
 import time
 
@@ -37,7 +37,7 @@ class CNN():
 
         # 读入图片
         img = Image.open(imgPath)
-        img = trans(img)  # 这里经过转换后输出的input格式是[C,H,W],网络输入还需要增加一维批量大小B
+        img = trans(img)  # 这里经过转换后输出的input格式是[C,H,W],网络输入还需要增加一维,指的是只输入了一张图片
         img = img.unsqueeze(0)  # 增加一维，输出的img格式为[1,C,H,W]
 
         # 判断计算机的GPUs是否可用
@@ -45,10 +45,11 @@ class CNN():
         if Use_gpu:
             self.__model = self.__model.cuda()
 
-        input = V(img.cuda())
+        input = Variable(img.cuda())
         score = self.__model(input)  # 将图片输入网络得到输出
-        probability = t.nn.functional.softmax(score, dim=1)  # 计算softmax，即该图片属于各类的概率
-        max_value, index = t.max(probability, 1)  # 找到最大概率对应的索引号，该图片即为该索引号对应的类别
+        print(score.data)
+        # 概率较大值对应的索引值，可看做预测结果
+        _, index = t.max(score.data, 1)
         print(index)
         resultClassNumber = str(index)[8]
         lsitName = ['决明子', '天花粉', '枳实', '苍术', '苍耳子', '黄连']
@@ -57,7 +58,7 @@ class CNN():
 if __name__ == '__main__':
     cnn = CNN()
     cnn.loadModel()
-    path = 'C:/Users/dell/Desktop/test/1.jpg'
+    path = 'C:/Users/dell/Desktop/test/5.jpg'
 
     time_open = time.time()
 
